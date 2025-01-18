d69475e095928f85f6f4766d000a2ade68075688
Razalpsuperio <razal@superiodigital.com>
2025-01-18 13:06:45 +0530

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { ToastProvider } from './context/ToastContext';
 export { ToastContainer } from './components/ToastContainer';
 export { useToast } from './hooks/useToast';
-export type { ToastType, ToastProps } from './types';
+export type { ToastProps, ToastType } from './types';
